d3f2be75c143a568d93381070780cce458d49122
LW-54 <leonardwilsonb@gmail.com>
2025-05-19 17:01:04 +0200

    0

--- devenv.nix
+++ devenv.nix
@@ -1,45 +1,15 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  languages.python = {
+    enable = true;
+    version = "3.11";
+    uv.enable = true;
+    uv.sync.enable = true;
+    venv.enable = true;
+  };
 
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
-
-  # https://devenv.sh/languages/
-  # languages.rust.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.cargo-watch.exec = "cargo-watch";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
-
-  enterShell = ''
-    hello
-    git --version
-  '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
+  packages = [
+    pkgs.cowsay
+  ];
 }
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from tipe!")
+
+
+if __name__ == "__main__":
+    main()
